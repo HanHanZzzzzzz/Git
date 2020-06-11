9eea15e6f9a7d1fa02b33f268c50cc4f2f6501ba
PoorTramp <903109165@qq.com>
2020-06-11 22:28:04 +0800

    this is a test3

--- Git/src/main/java/com/htp/HelloGit.java
+++ Git/src/main/java/com/htp/HelloGit.java
@@ -3,6 +3,12 @@ package com.htp;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+
+        System.out.println("lalal");
+        System.out.println("lalal");
+        System.out.println("lalal");
+        System.out.println("lalal");
+        System.out.println("分支添加");
     }
 }
 
